fe9036fd9a364df9a7fceb8c8991f44b4241afbb
Aditya Tupte <adityatupte2@gmail.com>
2026-02-20 16:46:53 +0000

    feat: Add submit snapshot functionality for buyer in land proposal process

--- programs/property_tokenization/src/instructions/add_land/mod.rs
+++ programs/property_tokenization/src/instructions/add_land/mod.rs
@@ -1,5 +1,7 @@
 pub mod buy_land_proposal;
 pub mod arbitrar_approval_to_buy;
+pub mod submint_snapshot;
 
 pub use buy_land_proposal::*;
 pub use arbitrar_approval_to_buy::*;
+pub use submint_snapshot::*;
---
+++ programs/property_tokenization/src/instructions/add_land/submint_snapshot.rs
@@ -0,0 +1,66 @@
+use anchor_lang::prelude::*;
+use crate::constant::*;
+use crate::errors::ErrorCode;
+use crate::state::{BuyLandProposalDetail, TransferLandDetail};
+
+#[derive(Accounts)]
+
+pub struct SubmitSnapshot<'info>{
+
+    #[account(
+        constraint = proposal.arbitrar_approval.contains(&signer.key()),
+        constraint = proposal.arbitrar_approved
+    )]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[
+            BUYPROPOSAL,
+            proposal.buyer_property_system.as_ref(),
+            &proposal.proposal_id.to_le_bytes(),
+        ],
+        bump = proposal.bump
+    )]
+
+    pub proposal : Account<'info,BuyLandProposalDetail>,
+
+}
+
+pub fn submit_snapshot_for_buyer(
+    ctx:Context<SubmitSnapshot>,
+    merkle_root : [u8;32],
+)->Result<()>{
+
+    let proposal = &mut ctx.accounts.proposal;
+
+    let current_time = Clock::get()?.unix_timestamp;
+
+    require!(!proposal.snapshot_submitted,ErrorCode::SnapshotAlreadySubmitted);
+
+
+    let one_day: i64 = 24 * 60 * 60;
+
+    let ten_days = one_day
+        .checked_mul(10)
+        .ok_or(ErrorCode::MathOverflow)?;
+
+    proposal.start_time = current_time
+        .checked_add(one_day)
+        .ok_or(ErrorCode::MathOverflow)?;
+
+    proposal.end_time = current_time
+        .checked_add(ten_days)
+        .ok_or(ErrorCode::MathOverflow)?;
+
+    proposal.merkle_root = merkle_root;
+
+    proposal.snapshot_submitted = true;
+
+    proposal.proposal_status = ProposalStatus::Active as u8;
+
+    
+    Ok(())
+
+
+}
