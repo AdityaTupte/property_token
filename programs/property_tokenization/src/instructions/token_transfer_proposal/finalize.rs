6dfba21324392d41da8baf7bb6feddbff34fa9d5
Aditya Tupte <adityatupte2@gmail.com>
2026-03-03 14:38:11 +0000

    feat: Add finalize functionality for safety proposal

---
+++ programs/property_tokenization/src/instructions/token_transfer_proposal/finalize.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+
+use crate::{constant::SAFETYPROPOSAL, functions::finalize, state::SafetyProposal};
+
+
+#[derive(Accounts)]
+pub struct Finalize<'info>{
+
+    #[account()]
+    pub signer:Signer<'info>,
+
+
+    #[account(
+        seeds=[
+            SAFETYPROPOSAL,
+            proposal.property_system.as_ref(),
+            &proposal.proposal_id.to_le_bytes(),
+        ],
+        bump = proposal.bump
+    )]
+    pub proposal : Account<'info,SafetyProposal>
+}
+
+
+pub fn finalize_sell_proposal(ctx:Context<Finalize>)->Result<()>{
+
+    let proposal = &mut *ctx.accounts.proposal; 
+
+    finalize(proposal)?;
+
+    Ok(())
+}
--- programs/property_tokenization/src/instructions/token_transfer_proposal/mod.rs
+++ programs/property_tokenization/src/instructions/token_transfer_proposal/mod.rs
@@ -2,8 +2,12 @@ pub mod use_safety_proposal;
 pub mod use_safety_proposal_arbitrar_vote;
 pub mod use_safety_submit;
 pub mod use_safety_voting;
+pub mod finalize;
 
 pub use use_safety_proposal::*;
 pub use use_safety_proposal_arbitrar_vote::*;
 pub use use_safety_submit::*;
 pub use use_safety_voting::*;
+pub use finalize::*;
+
+        
